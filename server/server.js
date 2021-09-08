99385522dc81a79952ecbe64dc61b1a5c9c1eda8
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2021-09-07 22:26:23 -0400

    added server and subfolders

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+/* ==== External Modules ==== */
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+/* ==== Internal Modules ==== */
+
+
+/* ==== Instanced Modules  ==== */
+const app = express(); // create express app
+
+/* ====  Configuration  ==== */
+
+
+app.use(cors());
+// to serve static files and to serve the react build
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+// JSON parsing middleware
+app.use(express.json());
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server started on port 5000");
+});

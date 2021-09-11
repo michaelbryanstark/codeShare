a9cd3059dff0b9b8143457e3dad766926ba19113
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2021-09-11 17:58:28 -0400

    updated routes and controllers for signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,2 +1,23 @@
 const db = require("../models");
 
+const createJWT = require("./helpers");
+
+const index = async (req, res) => {
+    const users = await db.User.find({});
+    console.log(users);
+    res.json(users);
+};
+
+const create = (req, res) => {
+    db.User.create(req.body, (err, savedUser) => {
+        if (err) return console.log("Error in User#create:", err);
+
+        const token = createJWT(savedUser);
+        return res.status(201).json({
+            message: "Success",
+            data: { token },
+        });
+    });
+};
+
+module.exports = { index, create };
